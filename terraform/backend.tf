81cddf55667dffa854db572b11be5f7639fe5cdd
kali <esetekaro@gmail.com>
2026-01-12 20:19:33 +0000

    commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tfstate-maro-threat1"
-    key            = "infra/terraform.tfstate"
+    key            = "infra/terraform-tfstate"
     region         = "us-east-2"
     profile        = "terraform"
     dynamodb_table = "terraform-locks"
